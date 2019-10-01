2e1440fc510a135d62171c94c5d7672443a81e88
teddyhn <tngo97@gmail.com>
2019-10-01 12:46:49 -0700

    /api/posts POST request endpoint functional

--- data/router.js
+++ data/router.js
@@ -2,6 +2,21 @@ const express = require('express');
 const db = require('./db');
 const router = express.Router();
 
+router.post('/', (req, res) => {
+    const { title, contents } = req.body;
+    if (!title || !contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post."})
+    }
+    else db.insert(req.body)
+        .then(post => {
+            res.status(201).send(post);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "There was an error while saving the post to the database." })
+        })
+})
+
 router.get('/', (req, res) => {
     db.find()
         .then(posts => {
