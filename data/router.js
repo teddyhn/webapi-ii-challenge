009319e55e73d9ff770032b2c5c1d5f5fef86839
teddyhn <tngo97@gmail.com>
2019-10-01 11:34:56 -0700

    /api/posts GET request endpoint functional

--- data/router.js
+++ data/router.js
@@ -2,5 +2,18 @@ const express = require('express');
 const db = require('./db');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    db.find()
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: "The posts information could not be retrieved."
+            })
+        })
+    
+})
 
 module.exports = router;
