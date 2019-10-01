9c1167a3af4bd27c0eaefdb9ac72696ce8a7b986
teddyhn <tngo97@gmail.com>
2019-10-01 13:03:41 -0700

    /api/posts/:id GET request endpoint functional

--- data/router.js
+++ data/router.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.post('/', (req, res) => {
     const { title, contents } = req.body;
     if (!title || !contents) {
-        res.status(400).json({ errorMessage: "Please provide title and contents for the post."})
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
     }
     else db.insert(req.body)
         .then(post => {
@@ -29,6 +29,20 @@ router.get('/', (req, res) => {
             })
         })
     
+});
+
+router.get('/:id', (req, res) => {
+    id = req.params.id;
+    db.findById(id)
+        .then(post => {
+            if (post.length === 0) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            } else res.status(200).send(post);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "The post information could not be retrieved." })
+        })
 })
 
 module.exports = router;
