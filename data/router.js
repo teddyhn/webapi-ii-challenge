ec48800b678998358b1c268afdf4c685efd20d15
teddyhn <tngo97@gmail.com>
2019-10-01 14:05:48 -0700

    /api/posts/:id PUT request endpoint functional, returns modified document

--- data/router.js
+++ data/router.js
@@ -98,4 +98,26 @@ router.delete('/:id', (req, res) => {
         })
 });
 
+router.put('/:id', (req, res) => {
+    id = req.params.id;
+    const { title, contents } = req.body;
+
+    if (!title || !contents) {
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    }
+    
+    else {
+        db.update(id, req.body)
+        .then(post => {
+            if (post.length === 0) {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            } else res.status(200).send(req.body);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: "The post information could not be modified." })
+        })
+    }
+});
+
 module.exports = router;
